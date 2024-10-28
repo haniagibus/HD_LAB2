c0cc9e00a7ca6d7911bdb0f7104435b2f1a67eb9
agatherat <kulesz.agnieszka@wp.pl>
2024-10-28 09:21:08 +0100

    poprawiony numer

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from faker import Faker
 from faker.providers import DynamicProvider
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta
 import csv
 import random
 import string
 
 specjalization_provider = DynamicProvider(
-    provider_name="medical_specialization",
-    elements=["Kardiolog", "Pediatra", "Ginekolog", "Dermatolog", "Neurolog",
-              "Ortopeda", "Chirurg", "Psychiatra", "Endokrynolog", "Onkolog",
-              "Okulista", "Laryngolog", "Reumatolog",
-              "Urolog", "Chirurg plastyczny", "Chirurg naczyniowy", "Medycyna rodzinna",
-              "Medycyna pracy", "Medycyna sportowa", "Pneumolog", "Immunolog",
-              "Patolog", "Radiolog", "Nefrolog", "Ginekolog-onkolog",
-              "Ortopeda dziecięcy", "Medycyna ratunkowa"],
+     provider_name="medical_specialization",
+     elements=[ "Kardiolog", "Pediatra", "Ginekolog", "Dermatolog", "Neurolog",
+    "Ortopeda", "Chirurg", "Psychiatra", "Endokrynolog", "Onkolog",
+    "Okulista", "Laryngolog", "Reumatolog",
+    "Urolog", "Chirurg plastyczny", "Chirurg naczyniowy", "Medycyna rodzinna",
+    "Medycyna pracy", "Medycyna sportowa", "Pneumolog", "Immunolog",
+    "Patolog", "Radiolog", "Nefrolog", "Ginekolog-onkolog",
+    "Ortopeda dziecięcy", "Medycyna ratunkowa"],
 )
 
 fake = Faker("pl_PL")
@@ -68,7 +68,6 @@ my_sentence_list = [
     'Pacjent ma napady paniki.'
 ]
 
-
 def generate_unique_identifier(existing_ids, length):
     """Generuje unikalny identyfikator o podanej długości."""
     while True:
@@ -77,7 +76,6 @@ def generate_unique_identifier(existing_ids, length):
             existing_ids.add(identifier)
             return identifier
 
-
 def generate_random_time_within_range(start_hour=7, end_hour=19):
     """Generuje losowy czas w przedziale od start_hour do end_hour."""
     hour = random.randint(start_hour, end_hour)
@@ -85,18 +83,9 @@ def generate_random_time_within_range(start_hour=7, end_hour=19):
     return f"{hour:02d}:{minute:02d}"
 
 
-def biased_random_int(low, high, bias=0.9):
-    if random.random() < bias:
-        # generate low number
-        return fake.random_int(min=low, max=(low + high) // 8)
-    else:
-        # generate high number
-        return fake.random_int(min=(low + high) // 8 + 1, max=high)
-
-
 existing_ids = set()
 
-# Pacjenci
+#Pacjenci
 with open("dane_pacjenci.csv", mode="w", newline="") as pacjenci_csv:
     writer = csv.writer(pacjenci_csv)
 
@@ -104,108 +93,47 @@ with open("dane_pacjenci.csv", mode="w", newline="") as pacjenci_csv:
         ID_pacjenta = fake.unique.random_int(100000000, 999999999)
         imie = fake.first_name()
         nazwisko = fake.last_name()
-        nr_telefonu = fake.phone_number()
+        nr_telefonu = fake.unique.numerify("### ### ###")
         miejscowosc = fake.city()
         ulica_i_numer_domu = fake.street_address()
         pesel = fake.unique.random_int(10000000000, 99999999999)
 
-        writer.writerow([ID_pacjenta, imie, nazwisko, nr_telefonu, miejscowosc, ulica_i_numer_domu, pesel])
+        writer.writerow([ID_pacjenta, imie, nazwisko, nr_telefonu, miejscowosc,ulica_i_numer_domu,pesel])
 
-# Recepty
-with open("dane_recepty.csv", mode="w", newline="") as recepty_csv:
-    writer = csv.writer(recepty_csv)
 
-    for i in range(100):
-        ID_recepty = fake.unique.random_int(100000000000000, 999999999999999)
-        waznosc = fake.random_int(30, 180, 30)
-        czy_wykupiona = fake.boolean()
-        data_wystawienia = fake.date_between((date.today() - timedelta(days=180)), date.today())
-
-        writer.writerow([ID_recepty, waznosc, czy_wykupiona, data_wystawienia])
-
-# Badania
-with open("dane_badania.csv", mode="w", newline="") as badania_csv:
-    writer = csv.writer(badania_csv)
-
-    test_names_provider = DynamicProvider(
-        provider_name="test_name",
-        elements=[
-            "morfologia krwi",
-            "tomografia komputerowa",
-            "badanie moczu",
-            "USG jamy brzusznej",
-            "EKG (elektrokardiogram)",
-            "EEG (elektroencefalogram)",
-            "test na cholesterol",
-            "badanie poziomu cukru",
-            "badanie wzroku",
-            "spirometria",
-            "badanie słuchu",
-            "próba wysiłkowa",
-            "RTG klatki piersiowej",
-            "mammografia",
-            "test na HIV",
-            "badanie poziomu witaminy D",
-            "test PSA",
-            "badanie TSH (tarczyca)",
-            "test na alergie",
-            "markery nowotworowe",
-            "biopsja skóry",
-            "densytometria (gęstość kości)",
-            "badanie kału",
-            "test wątrobowy",
-            "test nerkowy",
-            "badanie prostaty",
-            "test na anemię",
-            "glukoza na czczo",
-            "posiew bakteryjny",
-            "test na infekcje",
-            "badanie hormonów"
-        ],
-    )
-
-    fake.add_provider(test_names_provider)
-
-    for i in range(100):
-        nazwa_badania = fake.test_name()
-        czas_trwania = fake.random_int(5, 300, 5)
-        koszt = biased_random_int(0, 10000)
-
-        writer.writerow([nazwa_badania, czas_trwania, koszt])
-
-# Lekarze
+#Lekarze
 with open("dane_lekarze.csv", mode="w", newline="") as lekarze_csv:
     writer = csv.writer(lekarze_csv)
     for i in range(100):
-        Identyfikator = generate_unique_identifier(existing_ids, 12)
+        Identyfikator = generate_unique_identifier(existing_ids,12)
         imie = fake.first_name()
         nazwisko = fake.last_name()
         specjalizacja = fake.medical_specialization()
-        writer.writerow([Identyfikator, imie, nazwisko, specjalizacja])
+        writer.writerow([Identyfikator, imie, nazwisko,specjalizacja])
 
-# Wizyty
-start_date = datetime.strptime('2020-01-01', '%Y-%m-%d').date()
+#Wizyty
+start_date = datetime.strptime('2020-01-01','%Y-%m-%d').date()
 end_date = datetime.strptime('2024-01-01', '%Y-%m-%d').date()
 
-recepcjonistki = []
+recepcjonistki=[]
 for i in range(20):
-    recepcjonistki.append(generate_unique_identifier(existing_ids, 12))
+    recepcjonistki.append(generate_unique_identifier(existing_ids,12))
 
 with open("dane_wizyty.csv", mode="w", newline="") as wizyty_csv:
     writer = csv.writer(wizyty_csv)
     for i in range(1000):
-        ID_wizyty = generate_unique_identifier(existing_ids, 10)
-        data_umówienia = fake.date_between(start_date=start_date, end_date=end_date)
-        data_rozpoczęcia = fake.date_between(start_date=data_umówienia, end_date=data_umówienia + timedelta(days=180))
-        dolegliwosci = fake.sentence(ext_word_list=my_sentence_list)
+        ID_wizyty = generate_unique_identifier(existing_ids,10)
+        data_umówienia = fake.date_between(start_date=start_date,end_date=end_date)
+        data_rozpoczęcia = fake.date_between(start_date= data_umówienia,end_date=data_umówienia + timedelta(days=180))
+        dolegliwosci=fake.sentence(ext_word_list=my_sentence_list)
         assert len(dolegliwosci) <= 1000
-        kwota = round(random.uniform(40, 600), 2)
+        kwota=round(random.uniform(40, 600), 2)
         godzina = generate_random_time_within_range()
         czy_odbyta = random.choices(["TAK", "NIE"], weights=[0.7, 0.3], k=1)[0]
-        ID_recepcjonistki = random.choice(recepcjonistki)
-        writer.writerow(
-            [ID_wizyty, data_umówienia, data_rozpoczęcia, dolegliwosci, kwota, godzina, czy_odbyta, ID_recepcjonistki])
+        ID_recepcjonistki= random.choice(recepcjonistki)
+        writer.writerow([ID_wizyty, data_umówienia,data_rozpoczęcia,dolegliwosci, kwota, godzina,czy_odbyta,ID_recepcjonistki])
+
 
 # Oświadczamy, że treści wygenerowane przy pomocy z GenAI poddałyśmy krytycznej analizie i zweryfikowałyśmy.
 # Korzystałyśmy za zgodą prowadzącego z następujących narzędzi o potencjalnie wysokim stopniu ingerencji:
-# ChatGPT - wygenerowanie danych dla: my_sentence_list, specjalization_provider, test_names_provider
+# ChatGPT - wygenerowanie danych dla: my_sentence_list, specjalization_provider
